05a476dbcdd68fabd4929a30489f48d61a5f96d2
luvier05 <55365581+luvier05@users.noreply.github.com>
2019-10-01 23:08:30 -0500

    Add files via upload

---
+++ compTrans/src/comptrans/Menu.java
@@ -0,0 +1,13 @@
+
+package comptrans;
+
+
+public class Menu {
+
+   
+    public static void main(String[] args) {
+        
+    }
+    public 
+    
+}
---
+++ compTrans/src/comptrans/Tiquete.java
@@ -0,0 +1,82 @@
+
+package comptrans;
+
+public class Tiquete {
+    private int numeroViaje; 
+    private String  fecha;
+    private String  hora;
+    private String nomPas;
+    private String cos;
+
+    /**
+     * @return the numeroViaje
+     */
+    public int getNumeroViaje() {
+        return numeroViaje;
+    }
+
+    /**
+     * @param numeroViaje the numeroViaje to set
+     */
+    public void setNumeroViaje(int numeroViaje) {
+        this.numeroViaje = numeroViaje;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the hora
+     */
+    public String getHora() {
+        return hora;
+    }
+
+    /**
+     * @param hora the hora to set
+     */
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    /**
+     * @return the nomPas
+     */
+    public String getNomPas() {
+        return nomPas;
+    }
+
+    /**
+     * @param nomPas the nomPas to set
+     */
+    public void setNomPas(String nomPas) {
+        this.nomPas = nomPas;
+    }
+
+    /**
+     * @return the cos
+     */
+    public String getCos() {
+        return cos;
+    }
+
+    /**
+     * @param cos the cos to set
+     */
+    public void setCos(String cos) {
+        this.cos = cos;
+    }
+    
+    
+}
