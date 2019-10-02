9fedcbef3e062d9bf386f589dbc8ee183f49b556
luvier05 <55365581+luvier05@users.noreply.github.com>
2019-10-01 22:52:07 -0500

    Add files via upload

---
+++ maquinaria/src/Principal.java
@@ -0,0 +1,21 @@
+
+public class Principal {
+
+    public Principal(maquinas m) {
+        this.m = m;
+    }
+
+    public Principal() {
+    }
+    String cos ="";
+    Integer can=0;
+  
+    public void alquilar(){
+        can = Integer.parseInt(Menu.can.getText());
+        cos = Menu.cos.getText();
+                
+             
+    }
+    
+}
+
---
+++ maquinaria/src/maquinas.java
@@ -0,0 +1,64 @@
+
+public class maquinas {
+    private  String maquina; 
+    private String tipoMaq;
+    private String canMaquina;
+    private int cosMaquina;
+
+    /**
+     * @return the maquina
+     */
+    public String getMaquina() {
+        return maquina;
+    }
+
+    /**
+     * @param maquina the maquina to set
+     */
+    public void setMaquina(String maquina) {
+        this.maquina = maquina;
+    }
+
+    /**
+     * @return the tipoMaq
+     */
+    public String getTipoMaq() {
+        return tipoMaq;
+    }
+
+    /**
+     * @param tipoMaq the tipoMaq to set
+     */
+    public void setTipoMaq(String tipoMaq) {
+        this.tipoMaq = tipoMaq;
+    }
+
+    /**
+     * @return the canMaquina
+     */
+    public String getCanMaquina() {
+        return canMaquina;
+    }
+
+    /**
+     * @param canMaquina the canMaquina to set
+     */
+    public void setCanMaquina(String canMaquina) {
+        this.canMaquina = canMaquina;
+    }
+
+    /**
+     * @return the cosMaquina
+     */
+    public int getCosMaquina() {
+        return cosMaquina;
+    }
+
+    /**
+     * @param cosMaquina the cosMaquina to set
+     */
+    public void setCosMaquina(int cosMaquina) {
+        this.cosMaquina = cosMaquina;
+    }
+            
+}
